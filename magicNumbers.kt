af929fcd959153660adca1b0b86cb7243c1f0446
sharondagales78 <123050036+sharondagales78@users.noreply.github.com>
2023-02-22 23:35:44 -0600

    Create magicNumbers.kt
    03.2 Magic Dates
    The date June 10, 1960, is special because when we write it in the following format, the month times the day equals the year:
    6/10/60
    Write a program that asks the user to enter a month (in numeric form), a day, and a two-digit year. The program should then determine whether the month times the day is equal to the year. If so, it should display a message saying the date is magic. Otherwise, it should display a message saying the date is not magic.
    Write your code in a file named magicNumbers.kt in your unit03 folder. Turn in the GitHub link for a grade.

---
+++ magicNumbers.kt
@@ -0,0 +1,8 @@
+month=int(input('Enter month (numeric):'))
+day=int(input('Enter day:'))
+year=int(input('Enter two digit year:'))
+day_month=day*month
+if day_month==year;
+print('This date is magic!')
+else;
+print('This date is not magic.')
